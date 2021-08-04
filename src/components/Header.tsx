fd6a1ffc994684c690b46400e2ab3b0b4c8d16cb
rcelta <rcelta@contractor.ea.com>
2021-08-04 15:59:59 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+export const Header: React.FC<Props> = ()  => {
+    return (
+        <header>
+            <h1>Holis</h1>
+        </header>
+    )
+}
